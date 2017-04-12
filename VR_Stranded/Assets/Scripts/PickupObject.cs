1161b3b9649a6359646f309e6fe132ec2b878a0b
Madison Hicks <Madison Hicks>
2017-04-12 15:23:35 -0400

    combo clues, pickupables

---
+++ VR_Stranded/Assets/Scripts/PickupObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+	GameObject mainCamera;
+	bool carrying;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(carrying) {
+			carry(carriedObject);
+			checkDrop();
+			//rotateObject();
+		} else {
+			pickup();
+		}
+	}
+
+	void rotateObject() {
+		carriedObject.transform.Rotate(5,10,15);
+	}
+
+	void carry(GameObject o) {
+		o.transform.parent = GameObject.Find("Player").transform;
+		o.transform.rotation = Quaternion.identity;
+		o.transform.localEulerAngles = new Vector3 (90f, 90f, -90f);
+		o.transform.localPosition = new Vector3 (0f, 0.6f, 1f);
+	}
+
+	void pickup() {
+		if(Input.GetButton("Interact")) {
+			int x = Screen.width / 2;
+			int y = Screen.height / 2;
+
+			Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x,y));
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if(p != null) {
+					carrying = true;
+					carriedObject = p.gameObject;
+					//p.gameObject.rigidbody.isKinematic = true;
+					p.gameObject.GetComponent<Rigidbody>().useGravity = false;
+				}
+			}
+		}
+	}
+
+	void checkDrop() {
+		if(Input.GetButton("Back")) {
+			dropObject();
+		}
+	}
+
+	void dropObject() {
+		carrying = false;
+		//carriedObject.gameObject.rigidbody.isKinematic = false;
+		carriedObject.transform.parent = null;
+		carriedObject.gameObject.GetComponent<Rigidbody>().useGravity = true;
+		carriedObject = null;
+	}
+}
