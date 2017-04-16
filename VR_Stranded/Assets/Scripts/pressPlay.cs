5846102a82c48f51aeec6513248cb5a1a945c3c5
Madison Hicks <Madison Hicks>
2017-04-16 15:45:21 -0400

    kinda working

---
+++ VR_Stranded/Assets/Scripts/pressPlay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class pressPlay : MonoBehaviour {
+
+	bool playRange;
+	public int loading;
+	public GameObject highlight;
+
+
+	// Use this for initialization
+	void Start () {
+		playRange = false;
+		highlight.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playRange == true && Input.GetButton("Interact")){
+			Debug.Log ("Play");
+			Application.LoadLevel (loading);
+		}
+	}
+	void OnTriggerEnter(Collider col) {
+		
+		if (col.gameObject.tag == "Player") {
+			playRange = true;
+			Debug.Log ("Collide");
+			highlight.SetActive (true);
+		}
+	}
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.tag == "Player") {
+			playRange = false;
+			highlight.SetActive (false);
+			Debug.Log ("Not Collide");
+		}
+	}
+
+}
+
+
